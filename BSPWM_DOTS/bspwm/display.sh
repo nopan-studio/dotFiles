ab79b11c193f5f800402a832cdcd35e81f78fe41
nopan-studio <caneda.s.bscs@gmail.com>
2021-04-15 10:27:50 +0800

    2021 BSPWM DOTS

---
+++ BSPWM_DOTS/bspwm/display.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS1 --primary --mode 1366x768 --pos 900x410 --rotate normal --output DP1 --off --output HDMI1 --off --output VGA1 --mode 1440x900 --pos 0x0 --rotate left --output VIRTUAL1 --off
